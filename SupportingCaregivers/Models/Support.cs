f867937f2fa82cce5ea197cf8a29037bfe2f0e43
Mark Wong <markwong14@hotmail.com>
2019-03-30 13:13:24 -0400

    Created Support model

--- SupportingCaregivers/Models/Support.cs
+++ SupportingCaregivers/Models/Support.cs
@@ -5,5 +5,14 @@ using System.Threading.Tasks;
 
 namespace SupportingCaregivers.Models {
   public class Support {
+    public int Id { get; set; }
+    public String Caregiver { get; set; }
+    public String Patient { get; set; }
+    public String Behaviour { get; set; }
+    public String InterventionDone { get; set; }
+    public String SuccessRating { get; set; }
+    public DateTime DateTime { get; set; }
+    public String Location { get; set; }
+    public String Comments { get; set; }
   }
 }
